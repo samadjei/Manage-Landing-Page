4fe2a985319ceafe39e3712ce50743b16bec1bd9
Samuel Adjei <samueladj3i@gmail.com>
2020-06-04 19:29:01 +0100

    adding hamburger toggle

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@ new Glider(document.querySelector('.glider'), {
   slidesToShow: 3,
   draggable: true,
 });
+
+// Hamburger   Menu
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.menu');
+const overlay = document.querySelector('.bg-overlay');
+const links = document.querySelectorAll('.menu li');
+
+// Hamburger Menu
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  overlay.classList.toggle('open');
+  hamburger.classList.toggle('toggle');
+});
