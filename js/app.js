7b18ab5c52609ff468a783c14717ea1106a2a766
Samuel Adjei <samueladj3i@gmail.com>
2020-06-09 18:51:42 +0100

    completed testimonials section

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 new Glider(document.querySelector('.glider'), {
-  slidesToShow: 3,
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
   draggable: true,
+  dots: '#dots',
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 768,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
 });
+
+
+
 
 // Hamburger Menu 
 const btnHamburger = document.querySelector('#btnHamburger');
